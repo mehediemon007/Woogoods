a6a79abed17836f195abda434b58aaadbde4c203
mehediemon007 <mehediemon7@gmail.com>
2021-12-08 18:43:56 +0600

    first-commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+;(function($){
+
+    $(".banner-main-content").owlCarousel({
+        items:1,
+        dots:false,
+        nav:false
+    })
+
+    $(".banner-sub-content .sub-content-items").owlCarousel({
+        items:4,
+        dots:false,
+        nav:false,
+        margin:12
+    })
+
+    $(".six-column-slider").owlCarousel({
+        items:6,
+        dots:false,
+        nav:false,
+        margin:16
+    })
+
+})(jQuery);
