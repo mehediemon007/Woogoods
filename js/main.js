c0b3fd430c8d9a9552c3853564f7fa2fe0a51c18
mehediemon007 <mehediemon7@gmail.com>
2021-12-09 17:05:52 +0600

    home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 ;(function($){
 
+    $(".woo-categories-nav").click(function(){
+        $(this).toggleClass("active");
+    })
+
     $(".banner-main-content").owlCarousel({
         items:1,
         dots:false,
@@ -18,6 +22,30 @@
         dots:false,
         nav:false,
         margin:16
+    })
+
+    // Language Switcher  active
+
+    $(".lang-switcher").on("click", function(e){
+        e.preventDefault();
+        $(".lang-list").toggleClass("lang-list-open");
+      });
+      $(".lang-list li").each(function(){
+          $(this).on("click", function(){
+              var logoSrc = $(this).children(".flag").children("img").attr("src"); 
+              $(".lang-switcher > .flag img").attr("src", logoSrc);
+          });
+      });
+
+    //   Show More Content
+
+    $(".liked-products > .single-product").slice(0, 12).show();
+    $(".show-more-btn").on("click", function(e){
+        e.preventDefault()
+        $(".liked-products > .single-product:hidden").slice(0, 6).slideDown()
+        if ($(".liked-products > .single-product:hidden").length == 0) {
+            $(".show-more-btn").fadeOut('slow')
+        }
     })
 
 })(jQuery);
